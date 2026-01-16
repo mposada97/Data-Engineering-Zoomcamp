c07727ba0cb97e0cce1960e281b68fa1db09f84a
mposada97 <85133902+mposada97@users.noreply.github.com>
2026-01-16 22:25:59 +0000

    built docker image and pipeline file

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,7 +4,8 @@ import pandas as pd
 print('arguments', sys.argv)
 
 month = int(sys.argv[1])
-df = pd.Dataframe({"A": [1, 2], "B": [3, 4]})
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_{month}.parquet")
